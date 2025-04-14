2be9beb967de940d59ac65f583608e2a84edd924
Daniel <u2_danhru_waw@technischoosl>
2025-04-14 20:41:57 +0200

    panel, extra styles for cards & tag

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -1,4 +1,4 @@
-<div class="p-4 bg-white/5 rounded-xl flex gap-x-6 border border-transparent hover:border-blue-900 group transition-colors duration-1000">
+<x-panel class="flex gap-x-6">
     <div>
         <x-employer-logo></x-employer-logo>
     </div>
@@ -16,4 +16,4 @@
         <x-tag>Tag</x-tag>
         <x-tag>Tag</x-tag>
     </div>
-</div>
+</x-panel>
--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,4 +1,4 @@
-<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center border border-transparent hover:border-blue-900 group transition-colors duration-1000">
+<x-panel class="flex flex-col text-center">
     <div class="self-start text-sm">LARACAST</div>
 
     <div class="py-8">
@@ -8,10 +8,10 @@
 
     <div class="flex justify-between items-center mt-auto">
         <div>
-            <x-tag>Tag</x-tag>
-            <x-tag>Tag</x-tag>
-            <x-tag>Tag</x-tag>
+            <x-tag size="small">Backend</x-tag>
+            <x-tag size="small">Frontend</x-tag>
+            <x-tag size="small">Manager</x-tag>
         </div>
         <x-employer-logo :width="42"></x-employer-logo>
     </div>
-</div>
+</x-panel>
