4bcf8d726bc8b3f6477727cf0cc98347c46f1862
Daniel <u2_danhru_waw@technischoosl>
2025-04-14 21:35:42 +0200

    Migration to DB, tables to DB, model & factory

---
+++ app/Models/Job.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Job extends Model
+{
+
+    use HasFactory;
+
+    public function employer():belongsTo
+    {
+        return $this->belongsTo(Employer::class);
+    }
+
+    public function jobs():hasMany
+    {
+        return $this->hasMany(Job::class);
+    }
+}
