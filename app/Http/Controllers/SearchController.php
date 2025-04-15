62fefb397df8c54c6b7800af6054e06b71cc17ee
Daniel <u2_danhru_waw@technischoosl>
2025-04-15 12:00:16 +0200

    search

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Job;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
-    //
+    public function __invoke(){
+        $jobs = Job::where('title', 'LIKE', '%'.request('q').'%')->get();
+
+        return view('results', ['jobs' => $jobs]);
+    }
 }
